2b654719695deb5cdad342dcd10e2d827069a976
ElshanAli <elshanml@code.edu.az>
2022-07-25 20:07:02 +0400

    A part was added

---
+++ asset/JS/main.js
@@ -0,0 +1,6 @@
+let allCategory = document.querySelector('.buttom-dropdown');
+
+allCategory.addEventListener('click', function () {
+
+    document.querySelector('.all-cat-menu').classList.toggle('active-class')
+})
